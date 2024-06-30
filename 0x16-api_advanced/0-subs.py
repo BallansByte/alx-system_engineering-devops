c9d168177a59cbfcaf970c2078490874addfc2b0
BallansByte <Maballans@gmail.com>
2024-06-30 12:35:29 -0700

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}  # Set a custom User-Agent to avoid Too Many Requests error
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return 0
