5257c05bc97bb2bbbab0aaade31245a040489e06
BallansByte <Maballans@gmail.com>
2024-07-14 08:30:24 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 """
-0-subs
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0. 
-    Args:
-        subreddit (str): The name of the subreddit
-    Returns:
-        int: Number of subscribers or 0 if invalid subreddit
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User-Agent"}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-    except requests.RequestException:
-        return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
