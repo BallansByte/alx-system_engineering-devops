efcfca5876d8c530e64b89f870ccfbcfb302ee67
BallansByte <Maballans@gmail.com>
2024-07-14 10:15:53 -0700

    Task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,12 @@
 """Module for task 0"""
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(guns):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{guns}/about.json"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(guns),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
