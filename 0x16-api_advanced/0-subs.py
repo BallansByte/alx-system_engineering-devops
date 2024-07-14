f6c098678d1a2baa158104f48cb2279131937901
BallansByte <Maballans@gmail.com>
2024-07-14 09:52:14 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
+0-subs
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    
+    Args:
+        subreddit (str): The name of the subreddit
+    
+    Returns:
+        int: Number of subscribers or 0 if invalid subreddit
     """
-    url = 'http://www.reddit.com/r/memes/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Pytho(ALX 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    url = f"https://www.reddit.com/r/funny/about.json"
+    headers = {"User-Agent": "Custom User-Agent"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+    except requests.RequestException:
+        return 0
+
